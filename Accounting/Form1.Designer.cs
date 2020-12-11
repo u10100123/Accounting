2d5151c25eb8bfed5d40ca5cd0790ef984c3a102
u10100123 <45568808+u10100123@users.noreply.github.com>
2020-12-11 13:44:31 +0800

    Finish
    2020 12 11

--- Accounting/Form1.Designer.cs
+++ Accounting/Form1.Designer.cs
@@ -57,11 +57,15 @@ namespace Accounting
             this.button1.TabIndex = 1;
             this.button1.Text = "新增";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // monthCalendar1
             // 
             this.monthCalendar1.Location = new System.Drawing.Point(18, 18);
+            this.monthCalendar1.MaxSelectionCount = 1;
             this.monthCalendar1.Name = "monthCalendar1";
+            this.monthCalendar1.ShowToday = false;
+            this.monthCalendar1.ShowTodayCircle = false;
             this.monthCalendar1.TabIndex = 2;
             this.monthCalendar1.DateChanged += new System.Windows.Forms.DateRangeEventHandler(this.monthCalendar1_DateChanged);
             // 
@@ -81,6 +85,7 @@ namespace Accounting
             this.button2.TabIndex = 4;
             this.button2.Text = "1";
             this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // button3
             // 
@@ -88,8 +93,9 @@ namespace Accounting
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(60, 60);
             this.button3.TabIndex = 5;
-            this.button3.Text = "1";
+            this.button3.Text = "2";
             this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
             // button4
             // 
@@ -97,8 +103,9 @@ namespace Accounting
             this.button4.Name = "button4";
             this.button4.Size = new System.Drawing.Size(60, 60);
             this.button4.TabIndex = 6;
-            this.button4.Text = "1";
+            this.button4.Text = "3";
             this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
             // 
             // button5
             // 
@@ -106,8 +113,9 @@ namespace Accounting
             this.button5.Name = "button5";
             this.button5.Size = new System.Drawing.Size(60, 60);
             this.button5.TabIndex = 7;
-            this.button5.Text = "1";
+            this.button5.Text = "4";
             this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
             // 
             // button6
             // 
@@ -115,8 +123,9 @@ namespace Accounting
             this.button6.Name = "button6";
             this.button6.Size = new System.Drawing.Size(60, 60);
             this.button6.TabIndex = 8;
-            this.button6.Text = "1";
+            this.button6.Text = "5";
             this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
             // 
             // button7
             // 
@@ -124,8 +133,9 @@ namespace Accounting
             this.button7.Name = "button7";
             this.button7.Size = new System.Drawing.Size(60, 60);
             this.button7.TabIndex = 13;
-            this.button7.Text = "1";
+            this.button7.Text = "0";
             this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
             // 
             // button8
             // 
@@ -133,8 +143,9 @@ namespace Accounting
             this.button8.Name = "button8";
             this.button8.Size = new System.Drawing.Size(60, 60);
             this.button8.TabIndex = 12;
-            this.button8.Text = "1";
+            this.button8.Text = "9";
             this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
             // 
             // button9
             // 
@@ -142,8 +153,9 @@ namespace Accounting
             this.button9.Name = "button9";
             this.button9.Size = new System.Drawing.Size(60, 60);
             this.button9.TabIndex = 11;
-            this.button9.Text = "1";
+            this.button9.Text = "8";
             this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
             // 
             // button10
             // 
@@ -151,8 +163,9 @@ namespace Accounting
             this.button10.Name = "button10";
             this.button10.Size = new System.Drawing.Size(60, 60);
             this.button10.TabIndex = 10;
-            this.button10.Text = "1";
+            this.button10.Text = "7";
             this.button10.UseVisualStyleBackColor = true;
+            this.button10.Click += new System.EventHandler(this.button10_Click);
             // 
             // button11
             // 
@@ -160,8 +173,9 @@ namespace Accounting
             this.button11.Name = "button11";
             this.button11.Size = new System.Drawing.Size(60, 60);
             this.button11.TabIndex = 9;
-            this.button11.Text = "1";
+            this.button11.Text = "6";
             this.button11.UseVisualStyleBackColor = true;
+            this.button11.Click += new System.EventHandler(this.button11_Click);
             // 
             // label1
             // 
@@ -169,9 +183,8 @@ namespace Accounting
             this.label1.Font = new System.Drawing.Font("Microsoft JhengHei UI", 35F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
             this.label1.Location = new System.Drawing.Point(751, 12);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(320, 120);
+            this.label1.Size = new System.Drawing.Size(0, 120);
             this.label1.TabIndex = 14;
-            this.label1.Text = "$$$$$";
             // 
             // button12
             // 
@@ -190,8 +203,9 @@ namespace Accounting
             this.button13.Name = "button13";
             this.button13.Size = new System.Drawing.Size(191, 89);
             this.button13.TabIndex = 16;
-            this.button13.Text = "便利";
+            this.button13.Text = "清除";
             this.button13.UseVisualStyleBackColor = true;
+            this.button13.Click += new System.EventHandler(this.button13_Click);
             // 
             // richTextBox1
             // 
--- Accounting/Form1.cs
+++ Accounting/Form1.cs
@@ -16,10 +16,102 @@ namespace Accounting
         {
             InitializeComponent();
         }
+        class items
+        {
+            public string date { get; set; }
+            public string item { get; set; }
+            public int price { get; set; }
+        }
+        List<items> itm = new List<items>();
+        public int showprice=0;
 
         private void monthCalendar1_DateChanged(object sender, DateRangeEventArgs e)
         {
 
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string date = monthCalendar1.SelectionRange.Start.Date.ToShortDateString();
+            string item = textBox2.Text;
+            itm.Add(new items() { date = date, item = item, price = showprice});
+
+            richTextBox1.Text= "日期"+"\t"+"品項"+"\t"+"價格" + Environment.NewLine;
+            for (int i = itm.Count-1; i >-1; i--)
+            {
+                richTextBox1.Text += itm[i].date + "\t" + itm[i].item + " \t" + itm[i].price + Environment.NewLine;
+            }
+            showprice = 0;
+            label1.Text = "";
+            textBox2.Text = "";
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            showprice = showprice * 10 + 1;
+            label1.Text = showprice.ToString();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            showprice = showprice * 10 + 2;
+            label1.Text = showprice.ToString();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            showprice = showprice * 10 + 3;
+            label1.Text = showprice.ToString();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            showprice = showprice * 10 + 4;
+            label1.Text = showprice.ToString();
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            showprice = showprice * 10 + 5;
+            label1.Text = showprice.ToString();
+        }
+
+        private void button11_Click(object sender, EventArgs e)
+        {
+            showprice = showprice * 10 + 6;
+            label1.Text = showprice.ToString();
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            showprice = showprice * 10 + 7;
+            label1.Text = showprice.ToString();
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            showprice = showprice * 10 + 8;
+            label1.Text = showprice.ToString();
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            showprice = showprice * 10 + 9;
+            label1.Text = showprice.ToString();
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            showprice = showprice * 10;
+            label1.Text = showprice.ToString();
+        }
+
+        private void button13_Click(object sender, EventArgs e)
+        {
+            showprice = 0;
+            label1.Text = "";
+            textBox2.Text = "";
         }
     }
 }
